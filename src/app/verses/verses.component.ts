dba300980afd1ff19849215ec480ca26c0b48183
Werter Pires <werterpires23@hotmail.com>
2024-09-29 06:23:23 -0300

    add verses and verses on versions

--- src/app/verses/verses.component.ts
+++ src/app/verses/verses.component.ts
@@ -3,11 +3,12 @@ import { Verse } from './types';
 import { CommonModule } from '@angular/common';
 import { VersesService } from './verses.service';
 import { VersesOnVersionsComponent } from '../verses-on-versions/verses-on-versions.component';
+import { PropositionsComponent } from '../propositions/propositions.component';
 
 @Component({
   selector: 'app-verses',
   standalone: true,
-  imports: [CommonModule, VersesOnVersionsComponent],
+  imports: [CommonModule, VersesOnVersionsComponent, PropositionsComponent],
   templateUrl: './verses.component.html',
   styleUrl: './verses.component.css',
 })
@@ -20,11 +21,24 @@ export class VersesComponent {
   selectVerse(verse: Verse) {
     this.selectedVerse = verse;
 
-    this.versesService
-      .getVersesOnVersionsByVerseId(verse.verseId)
-      .subscribe((versesOnVersions) => {
+    this.versesService.getVersesOnVersionsByVerseId(verse.verseId).subscribe({
+      next: (versesOnVersions) => {
         verse.verseOnVersions = versesOnVersions;
         this.selectedVerse = verse;
-      });
+      },
+      error: (error) => {
+        throw error;
+      },
+    });
+
+    this.versesService.getPropositionsByVerseId(verse.verseId).subscribe({
+      next: (propositions) => {
+        verse.propositions = propositions;
+        this.selectedVerse = verse;
+      },
+      error: (error) => {
+        throw error;
+      },
+    });
   }
 }
--- src/app/verses/verses.service.ts
+++ src/app/verses/verses.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { VerseOnVersion } from '../verses-on-versions/types';
+import { Proposition } from '../propositions/types';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,18 @@ export class VersesService {
     return this.httpClient
       .get<VerseOnVersion[]>(
         `http://localhost:3000/verses-on-versions/verse/${verseId}/version_abbr`
+      )
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return throwError(() => error);
+        })
+      );
+  }
+
+  getPropositionsByVerseId(verseId: number): Observable<Proposition[]> {
+    return this.httpClient
+      .get<Proposition[]>(
+        `http://localhost:3000/propositions/verse/${verseId}/proposition_id`
       )
       .pipe(
         catchError((error: HttpErrorResponse) => {
