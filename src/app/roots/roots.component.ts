01f0e4f464c5939bbcb60584923899d4e849b5b7
Werter Pires <werterpires23@hotmail.com>
2024-10-18 09:27:44 -0300

    add roots component

---
+++ src/app/roots/roots.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roots',
+  standalone: true,
+  imports: [],
+  templateUrl: './roots.component.html',
+  styleUrl: './roots.component.css'
+})
+export class RootsComponent {
+
+}
