bde066eb2fb10ee9f6c394e61a49190a901f7762
Werter Pires <werterpires23@hotmail.com>
2024-10-18 09:32:00 -0300

    add originals component

---
+++ src/app/originals/originals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-originals',
+  standalone: true,
+  imports: [],
+  templateUrl: './originals.component.html',
+  styleUrl: './originals.component.css'
+})
+export class OriginalsComponent {
+
+}
